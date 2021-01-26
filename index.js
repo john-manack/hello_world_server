7fc9da98a56c2c9e7f75187a0ddb2abe66ff5d10
John Manack <jmnck28@gmail.com>
2021-01-26 16:33:44 -0500

    completes exercise by creating express server which prints Hello World

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 'use strict';
+const HTTP = require("http");
+
+const HOSTNAME = '127.0.0.1';
+const PORT = 3000;
+
+const express = require("express");
+const app = express();
+
+const SERVER = HTTP.createServer(app);
+
+SERVER.listen(PORT, HOSTNAME, () => {
+    console.log(`Server is running at http://${HOSTNAME}:${PORT}/`);
+})
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
